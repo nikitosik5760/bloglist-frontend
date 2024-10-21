ae7a313b43354f042225f5321c23fdf4cfbf0771
NikitaGavr <nikita2gavrilenko@gmail.com>
2024-10-21 16:53:31 +0300

    create blog to separate component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import './global.css'
 import { Notification } from './components/Notification'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [succesMessage, setSuccesMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
 
@@ -20,7 +18,7 @@ const App = () => {
   useEffect(() => {
     const getBlogs = async() => {
       const blogs = await blogService.getAll()
-      setBlogs( blogs )
+      setBlogs(blogs)
     }
     getBlogs()
   }, [])
@@ -63,26 +61,7 @@ const App = () => {
     blogService.setToken(null)
   }
 
-  const handleBlogCreation = async(e) => {
-    e.preventDefault()
-    try {
-      const createdblog =  await blogService.create({ title, author, url })
-      blogs.push(createdblog)
-      setAuthor('')
-      setTitle('')
-      setUrl('')
-      setSuccesMessage(`a new blog ${createdblog.title} added`)
-      setTimeout(() => {
-        setSuccesMessage(null)
-      }, 15000)
-    } catch (exeption) {
-      setErrorMessage(exeption.toString())
-      setTimeout(()=>{
-        setErrorMessage(null)
-      }, 15000)
-      return console.log('exeption', exeption)
-    }
-  }
+
 
   const login = () => (
     <form onSubmit={handleLogin}>
@@ -101,7 +80,7 @@ const App = () => {
         >
         </input>
       </div>
-      <button type='submit'>submit</button>
+      <button type='submit'>login</button>
     </form>
   )
 
@@ -123,30 +102,11 @@ const App = () => {
       <p>{user.name} logged in</p>
       <button onClick={handleLogout}>logout</button>
       <h2>create new</h2>
-      <form onSubmit={handleBlogCreation}>
-        <div>
-          title:
-          <input
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-          ></input>
-        </div>
-        <div>
-          author:
-          <input
-          value={author}
-          onChange={({ target }) => setAuthor(target.value)}
-          ></input>
-        </div>
-        <div>
-          url:
-          <input
-          value={url}
-          onChange={({ target }) => setUrl(target.value)}
-          ></input>
-        </div>
-        <button type='submit'>create</button>
-      </form>
+      <BlogForm 
+      setBlogs={setBlogs} 
+      setSuccesMessage={setSuccesMessage}
+      setErrorMessage={setErrorMessage}
+      ></BlogForm>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ src/components/BlogForm.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import blogService from '../services/blogs'
+
+const BlogForm = ({setBlogs, setSuccesMessage, setErrorMessage}) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleBlogCreation = async(e) => {
+    e.preventDefault()
+    try {
+      const createdblog = await blogService.create({ title, author, url })
+      setBlogs(b=>b.concat(createdblog))
+      setAuthor('')
+      setTitle('')
+      setUrl('')
+      setSuccesMessage(`a new blog ${createdblog.title} added`)
+      setTimeout(() => {
+        setSuccesMessage(null)
+      }, 15000)
+    } catch (exeption) {
+      setErrorMessage(exeption.toString())
+      setTimeout(()=>{
+        setErrorMessage(null)
+      }, 15000)
+      return console.log('exeption', exeption)
+    }
+  }
+
+  return (
+    <form onSubmit={handleBlogCreation}>
+        <div>
+          title:
+          <input
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+          ></input>
+        </div>
+        <div>
+          author:
+          <input
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+          ></input>
+        </div>
+        <div>
+          url:
+          <input
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+          ></input>
+        </div>
+        <button type='submit'>create</button>
+      </form>
+  )
+}
+
+export default BlogForm
