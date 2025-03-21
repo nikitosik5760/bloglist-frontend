72d0e1d2e03212e698c6598e5c3bcc7be9908e95
NikitaGavr <nikita2gavrilenko@gmail.com>
2025-03-21 10:05:25 +0100

    added eslint and prop types; added proptypes to Toggle component

--- src/components/Togglable.jsx
+++ src/components/Togglable.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React, { forwardRef, useImperativeHandle, useState } from "react";
 
 const Togglable = forwardRef((props, refs) => {
@@ -28,5 +29,12 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string,
+  children: PropTypes.element,
+};
+
+Togglable.displayName = "Togglable";
 
 export default Togglable;
